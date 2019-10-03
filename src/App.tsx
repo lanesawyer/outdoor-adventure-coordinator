a8d868b35c33a641402f11653d6f26e56ffa88df
Lane Sawyer <lanesawyer@gmail.com>
2019-10-02 18:52:17 -0700

    Adding styled components, cleaning up, and very basic gear entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 // @ts-ignore
 import { Pane, Heading, TextInputField, Button } from 'evergreen-ui';
 
 const App: React.FC = () => {
+
+  const [newGear, setNewGear] = React.useState<string>('');
+
+  const [gearList, setGearList] = React.useState<Array<string>>(
+   JSON.parse(localStorage.getItem('gearList')!) || []
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem('gearList', JSON.stringify(gearList));
+  }, [gearList]);
+
   return (
     <div className="App">
       <header className="App-header">
         <Pane>
           <Heading size={900}>Outdoor Adventure Coordinator</Heading>
           <TextInputField
+            value={newGear}
+            onChange={(e: any) => setNewGear(e.target.value)}
             label="Equipment"
             placeholder="Add equipment"
           />
-          <Button appearance="primary">Add</Button>
-
-          
+          <Button appearance="primary" onClick={() => setGearList(gearList.concat(newGear))}>Add</Button>
         </Pane>
       </header>
+      <Pane>
+        <ul>
+          { gearList.map((gear) => <li>{gear}</li>) }
+        </ul>
+      </Pane>
     </div>
   );
 }
